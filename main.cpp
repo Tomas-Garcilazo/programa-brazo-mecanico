c8ae2dbbfe7922f1f1dad470f8c3585961f4b541
Tomás Garcilazo <tomasGarcilazo@outlook.com.ar>
2019-10-16 19:59:53 -0300

    cambie el programa para que ahora la matriz sea de int

--- main.cpp
+++ main.cpp
@@ -15,9 +15,11 @@ using namespace std;
 
 
 
-void mostrar_mapa(char mapa[49][65]){
-for (int i = 0; i < 49; i++){
-    cout << mapa[i] << endl;}
+
+void mostrar_mapa(int mapa[49][65]){
+	for (int i = 0; i < 49; i++){
+	    cout << mapa[i] << endl;
+	}
 }
 
 int main ( int argc, char** argv )
@@ -27,14 +29,7 @@ int main ( int argc, char** argv )
     /// siempre que leas "mapa" se refiere a mapa de pixeles
     /// lo mas importante es la matriz "char mapa" de abajo
     ///el archivo se crea cuando el programa SE CIERRA
-    char mapa[TAM_Y][TAM_X];
-
-    for (int j = 0; j<49; j++){
-        for (int i = 0; i < 64; i++){
-            mapa[j][i] = '#';
-        }
-        mapa[j][64] = '\0';
-    }
+    int mapa[TAM_Y][TAM_X]={0};
 
     int x, y;
     bool buttonState  = false;
@@ -70,7 +65,7 @@ int main ( int argc, char** argv )
                 if (x > -1 && x <640 && y > -1 && y <480)
                     x = (int) x/10;
                     y = (int) y/10;
-                    mapa[y][x] = ' ';
+                    mapa[y][x] = 1;
             	break;
             }
         }
@@ -78,5 +73,10 @@ int main ( int argc, char** argv )
     }
 
     mostrar_mapa(mapa);
+    FILE *f;
+    f = fopen("archivo_matriz.dat", "wb");
+    fwrite(mapa, sizeof(int)*65*49, 1, f);
+    cout << sizeof (mapa);
+
     return 0;
 }
